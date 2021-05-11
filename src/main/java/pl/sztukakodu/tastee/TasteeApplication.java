9ef45d9ce7cf80fd74bebee220c90cdee49b8bd8
Dariusz Mydlarz <dmydlarz@gmail.com>
2021-05-11 10:21:28 +0200

    Init project

---
+++ src/main/java/pl/sztukakodu/tastee/TasteeApplication.java
@@ -0,0 +1,13 @@
+package pl.sztukakodu.tastee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasteeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sztukakodu/tastee/TasteeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sztukakodu.tastee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
