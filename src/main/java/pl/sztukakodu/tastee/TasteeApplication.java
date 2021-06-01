bbb583b5cb8ff958a631ab1f96ceb1000d880497
Dariusz Mydlarz <dmydlarz@gmail.com>
2021-06-01 10:17:19 +0200

    4.6 - Long Task Timer

--- src/main/java/pl/sztukakodu/tastee/TasteeApplication.java
+++ src/main/java/pl/sztukakodu/tastee/TasteeApplication.java
@@ -5,8 +5,10 @@ import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TasteeApplication {
 
 	public static void main(String[] args) {
